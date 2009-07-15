3bae63d33ec1dcb4125210c52838e0619dcb1836
Robert Walker <robert4723@me.com>
2009-07-15 13:22:53 -0400

    Fixed failing specs for welcome index view.

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -5,8 +5,17 @@ describe "/welcome/index" do
     render 'welcome/index'
   end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/welcome/index])
+  it "should contain the 'sidebar' div" do
+    response.should have_tag('div#sidebar')
+  end
+  
+  it "should contain the 'blog_post_container' div" do
+    response.should have_tag('div#blog_post_container') do
+      with_tag('div.post_content') do
+        with_tag('h2.post_title')
+        with_tag('div.post_date_line')
+        with_tag('div.post_narrative')
+      end
+    end
   end
 end
