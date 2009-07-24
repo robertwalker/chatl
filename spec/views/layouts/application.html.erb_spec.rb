5402a539960b7d3fd20b0aa6a081fc2348de8fd2
Robert Walker <robert4723@me.com>
2009-07-23 20:27:14 -0400

    Added view specs for application layout.

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -5,10 +5,30 @@ describe "/layouts/application" do
     render 'layouts/application'
   end
 
+  it "should contain the 'html' tag" do
+    response.should have_tag('html[xmlns=?][xml:lang=?][lang=?]',
+                             "http://www.w3.org/1999/xhtml", "en", "en")
+  end
+
+  it "should contain the head tag" do
+    response.should have_tag('head') do
+      with_tag('meta[http-equiv=?][content=?]', "Content-Type", "text/html; charset=utf-8")
+      with_tag('title', /^Cocoaheads Atlanta.*/)
+    end
+  end
+
+  it "should contain the body tag" do
+    response.should have_tag('body')
+  end
+
+  it "should contain the banner div" do
+    response.should have_tag('div#banner')
+  end
+
   it "should contain the 'sidebar' div" do
     response.should have_tag('div#sidebar')
   end
-  
+
   it "should contain the 'main_content' div" do
     response.should have_tag('div#main_content')
   end
