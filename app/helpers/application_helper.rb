fa0b9a5e1d2529f059f3644fe925e0d5f2e10f50
Robert Walker <robert4723@me.com>
2009-08-19 13:38:45 -0400

    Tab bar now auto-highlights based on URI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def tab_class_for(base_uri)
+    if request.request_uri.starts_with?("/#{base_uri}")
+      'tab selected_tab'
+    else
+      'tab'
+    end
+  end
 end
