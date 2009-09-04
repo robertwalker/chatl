e6e5aa5d94981a95348258d7792a93bf55ecd776
Robert Walker <robert4723@me.com>
2009-09-04 17:58:02 -0400

    Now able to send emails for scheduled meeting and a reminder.

--- app/models/meeting_mailer.rb
+++ app/models/meeting_mailer.rb
@@ -1,19 +1,21 @@
 class MeetingMailer < ActionMailer::Base
+  helper :application 
+
   def scheduled_notification(meeting)
     subject    "The #{month_name(meeting)} CocoaHeads Atlanta meeting has been scheduled"
-    recipients 'list@cocoaheadsatlanta.org'
-    from       'Michael L. Ward <mikeyward@gmail.com>'
+    recipients 'Robert Walker <robert348@me.com>'
+    from       'Robert Walker <robert4723@me.com>'
     sent_on    Time.now
-    
+
     body       :meeting => meeting
   end
 
   def reminder(meeting)
     subject    "Reminder: CocoaHeads Atlanta meeting is tomorrow"
-    recipients 'list@cocoaheadsatlanta.org'
-    from       'Michael L. Ward <mikeyward@gmail.com>'
+    recipients 'Robert Walker <robert348@me.com>'
+    from       'Robert Walker <robert4723@me.com>'
     sent_on    Time.now
-    
+
     body       :meeting => meeting
   end
 
