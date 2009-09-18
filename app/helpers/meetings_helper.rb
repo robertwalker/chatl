5c2a990a4debee9bbbf6127175ad586bb4e8e432
Robert Walker <robert4723@me.com>
2009-09-18 17:12:35 -0400

    Fixed to prevent RSVP changes to past meetings.

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,8 +1,28 @@
 module MeetingsHelper
   def rsvp_state
     current_attendee = @meeting.attendee_with_user(current_user)
+    klass = rsvp_state_style(current_attendee.rsvp)
+    content_tag(:p, current_attendee.rsvp, :class => klass)
+  end
+
+  def meeting_attendance
+    yes_rsvps = @meeting.attendees.find_all_by_rsvp("Yes")
+    no_rsvps = @meeting.attendees.find_all_by_rsvp("No")
+    maybe_rsvps = @meeting.attendees.find_all_by_rsvp("Maybe")
+
+    content_tag(:p, "#{yes_rsvps.count} Yes", :class => rsvp_state_style("Yes")) +
+    content_tag(:p) do
+      content_tag(:span, "#{no_rsvps.count} No", :class => "attendee_rsvp_no") +
+      content_tag(:span, " / ") +
+      content_tag(:span, "#{maybe_rsvps.count} Maybe", :class => "attendee_rsvp_maybe")
+    end
+  end
+
+  protected
+
+  def rsvp_state_style(rsvp)
     klass = "attendee_rsvp"
-    case current_attendee.rsvp
+    case rsvp
     when "Yes"
       klass << " attendee_rsvp_yes"
     when "No"
@@ -10,6 +30,5 @@ module MeetingsHelper
     when "Maybe"
       klass << " attendee_rsvp_maybe"
     end
-    content_tag(:p, current_attendee.rsvp, :class => klass)
   end
 end
