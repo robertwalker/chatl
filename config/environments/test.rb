c9c779c5d7fc995bdf2a8c5a360212b01eda7b97
Robert Walker <robert4723@me.com>
2009-10-13 13:32:22 -0400

    Updated for rSpec 1.2.9 and Cucumber 0.4.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,9 +28,9 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
 # Gem dependencies for test environment
-config.gem 'rspec', :lib => false, :version => '>=1.2.8'
-config.gem 'rspec-rails', :lib => false, :version => '>=1.2.7.1'
-config.gem 'cucumber', :lib => false, :version => '>=0.3.104'
+config.gem 'rspec', :lib => false, :version => '>=1.2.9'
+config.gem 'rspec-rails', :lib => false, :version => '>=1.2.9'
+config.gem 'cucumber', :lib => false, :version => '>=0.4'
 config.gem 'webrat', :lib => false, :version => '>=0.5.3'
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", 
                                       :version => ">=1.2.2", 
