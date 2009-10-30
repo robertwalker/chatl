2e4445c4e8a9ac56334f59c047d01402ea0e3d66
Robert Walker <robert4723@me.com>
2009-10-30 14:20:33 -0400

    Allows only admins to create, edit, or destroy data_files.

--- spec/views/data_files/index.html.erb_spec.rb
+++ spec/views/data_files/index.html.erb_spec.rb
@@ -24,4 +24,54 @@ describe "/data_files/index.html.erb" do
     response.should have_tag("tr>td", "value for name".to_s, 2)
     response.should have_tag("tr>td", "value for content_type".to_s, 2)
   end
+
+  it "should not render show data_file link" do
+    render
+    response.should_not have_tag("a[href=?]", %r{/data_files/\d+}, "Show")
+  end
+
+  it "should not render edit data_file link" do
+    render
+    response.should_not have_tag("a[href=?]", %r{/data_files/\d+/edit}, "Edit")
+  end
+
+  it "should not render destroy link" do
+    render
+    response.should_not have_tag("a[href=?]", %r{/data_files/\d+}, "Destroy")
+  end
+
+  it "should not render new link" do
+    render
+    response.should_not have_tag("a[href=?]", "/data_files/new")
+  end
+
+  describe "logged in as an admin" do
+    before(:each) do
+      admin_role = Factory(:role)
+      @admin_user = Factory.build(:user)
+      @admin_user.roles << admin_role
+      @admin_user.save
+      login_as(@admin_user)
+    end
+
+    it "renders show data_file link" do
+      render
+      response.should have_tag("a[href=?]", %r{/data_files/\d+}, "Show")
+    end
+
+    it "renders edit data_file link" do
+      render
+      response.should have_tag("a[href=?]", %r{/data_files/\d+/edit}, "Edit")
+    end
+
+    it "renders destroy link" do
+      render
+      response.should have_tag("a[href=?]", %r{/data_files/\d+}, "Destroy")
+    end
+
+    it "renders new data_file link" do
+      render
+      response.should have_tag("a[href=?]", "/data_files/new")
+    end
+  end
 end
