7dd4852a8f45a415c86e5aa4d017b79e5f8fbe60
Robert Walker <robert4723@me.com>
2010-02-01 18:42:49 +0000

    Fixed bug with default calculation of meeting date/time.

--- config/initializers/time_extentions.rb
+++ config/initializers/time_extentions.rb
@@ -1,7 +1,7 @@
 class Time
-  def self.time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_of_day = 19, min_of_hour = 0)
+  def self.time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_offset = 24, minute_offset = 0)
     offsets = [ 6, 5, 4, 3, 2, 1, 0].rotate(wday + 1)
-    first = Time.now.next_month.beginning_of_month
-    first + offsets[first.wday].days + (wnum - 1).week + hour_of_day.hours + min_of_hour.minutes
+    first = Time.now.utc.next_month.beginning_of_month
+    first + offsets[first.wday].days + (wnum - 1).week + hour_offset.hours + minute_offset.minutes
   end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Meeting do
-  def time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_of_day = 19, min_of_hour = 0)
+  def time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_of_day = 24, min_of_hour = 0)
     offsets = [ 6, 5, 4, 3, 2, 1, 0].rotate(wday + 1)
-    first = Time.now.next_month.beginning_of_month
+    first = Time.now.utc.next_month.beginning_of_month
     first + offsets[first.wday].days + (wnum - 1).week + hour_of_day.hours + min_of_hour.minutes
   end
 
