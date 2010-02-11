7d3eba545822320d2f2789ce8036dfceb20873e8
Robert Walker <robert4723@me.com>
2010-02-11 12:19:03 -0500

    Refactor: Moved meeting_at into a method used to "humanize" the reminder notification.

--- app/models/meeting_mailer.rb
+++ app/models/meeting_mailer.rb
@@ -12,21 +12,26 @@ class MeetingMailer < ActionMailer::Base
 
   def reminder(meeting)
     days_from_now = meeting.scheduled_at.to_date - Time.now.to_date
-    case days_from_now
-    when 7
-      meeting_at = "in one week"
-    when 1
-      meeting_at = "tomorrow"
-    when 0
-      meeting_at = "today"
-    else
-      meeting_at = "#{days_from_now} days"
-    end
+    meeting_at = humanize_meeting_at(days_from_now)
     subject    "Reminder: #{meeting.title} is #{meeting_at}"
     recipients 'Robert Walker <robert4723@me.com>'
     from       'CocoaHeads Atlanta <noreply@cocoaheadsatlanta.org>'
     sent_on    Time.now
 
     body       :meeting => meeting, :meeting_at => meeting_at
+  end
+
+  private
+  def humanize_meeting_at(days_from_now)
+    case days_from_now
+    when 7
+      "coming up in one week"
+    when 1
+      "tomorrow"
+    when 0
+      "today"
+    else
+      "coming up in #{days_from_now} days"
+    end
   end
 end
