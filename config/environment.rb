9ec434d1c7dc2eeb092cf3f24b0b7491ad649983
Robert Walker <robert4723@me.com>
2010-03-05 15:45:25 -0500

    Added will_paginate gem dependency.

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem 'aasm', :version => '>=2.1.5'
   config.gem 'RedCloth', :version => '>=4.2.2'
   config.gem 'gravtastic', :version => '>= 2.2.0'
+  config.gem 'will_paginate', :version => '>= 2.3.11'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
