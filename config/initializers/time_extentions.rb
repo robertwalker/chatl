559beacb1cd440cfe196be2946e49014faf356c1
Robert Walker <robert4723@me.com>
2010-04-22 13:05:59 -0400

    Fixed default meeting time caused by daylight saving time issue.

--- config/initializers/time_extentions.rb
+++ config/initializers/time_extentions.rb
@@ -1,7 +1,9 @@
 class Time
-  def self.time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_offset = 24, minute_offset = 0)
+  def self.time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_offset = 19, minute_offset = 0)
     offsets = [ 6, 5, 4, 3, 2, 1, 0].rotate(wday + 1)
-    first = Time.now.utc.next_month.beginning_of_month
-    first + offsets[first.wday].days + (wnum - 1).week + hour_offset.hours + minute_offset.minutes
+    localtime = Time.now.in_time_zone
+    first = localtime.next_month.beginning_of_month
+    meeting_time = first + offsets[first.wday].days + (wnum - 1).week +
+                   hour_offset.hours + minute_offset.minutes
   end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,10 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Meeting do
-  def time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_of_day = 24, min_of_hour = 0)
+  def time_at_wnum_wday_hour(wnum = 2, wday = 4, hour_offset = 19, minute_offset = 0)
     offsets = [ 6, 5, 4, 3, 2, 1, 0].rotate(wday + 1)
-    first = Time.now.utc.next_month.beginning_of_month
-    first + offsets[first.wday].days + (wnum - 1).week + hour_of_day.hours + min_of_hour.minutes
+    localtime = Time.now.in_time_zone
+    first = localtime.next_month.beginning_of_month
+    meeting_time = first + offsets[first.wday].days + (wnum - 1).week +
+                   hour_offset.hours + minute_offset.minutes
   end
 
   before(:each) do
