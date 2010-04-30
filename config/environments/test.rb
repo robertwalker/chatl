500555378b726d6a6b795062f8809eb120d35cd3
Robert Walker <robert4723@me.com>
2010-04-30 14:20:44 -0400

    Upgraded to Webrat 0.7.1.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,5 +32,5 @@ config.gem 'rspec', :lib => false, :version => '>=1.3.0'
 config.gem 'rspec-rails', :lib => false, :version => '>=1.3.2'
 config.gem 'cucumber', :lib => false, :version => '>=0.6.4'
 config.gem 'cucumber-rails', :lib => false, :version => '>=0.3.0'
-config.gem 'webrat', :lib => false, :version => '>=0.7.0'
+config.gem 'webrat', :lib => false, :version => '>=0.7.1'
 config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => ">=1.2.2"
