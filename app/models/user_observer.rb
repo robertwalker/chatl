151baf4f49fffb5370d99dbc0b317cd905f436d7
Robert Walker <robert4723@me.com>
2010-05-11 19:49:29 -0400

    Fixed: Requesting access to the "chatter" mailing list from user edit page.
    Fixed: The identity_url input was mistakenly removed from the new user
           form in previous commit.

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -3,10 +3,15 @@ class UserObserver < ActiveRecord::Observer
     UserMailer.deliver_signup_notification(user) unless user.identity_url.ends_with?("_pending")
   end
 
+  def after_validation(user)
+    if user.subscribe_to_chatter_changed?
+      UserMailer.deliver_chatter_list(user) if user.subscribe_to_chatter?
+    end
+  end
+
   def after_save(user)
     if user.recently_activated?
       UserMailer.deliver_activation(user)
-      UserMailer.deliver_chatter_list(user) if user.subscribe_to_chatter?
     end
   end
 end
