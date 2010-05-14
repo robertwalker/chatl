4388acfa2c8edcf05b3281abdb3da795fa2b9f3b
Robert Walker <robert4723@me.com>
2010-05-14 14:11:30 -0400

    Fixed: W3C validation errors in data_files index page.

--- app/helpers/data_files_helper.rb
+++ app/helpers/data_files_helper.rb
@@ -2,17 +2,17 @@ module DataFilesHelper
   def data_file_icon_src(mime_type)
     case mime_type
     when 'image/jpeg'
-      'images/jpeg_file.png'
+      'jpeg_file.png'
     when 'image/gif'
-      'images/gif_file.png'
+      'gif_file.png'
     when 'image/png'
-      'images/png_file.png'
+      'png_file.png'
     when 'image/tiff'
-      'images/tiff_file.png'
+      'tiff_file.png'
     when 'application/pdf'
-      'images/pdf_file.png'
+      'pdf_file.png'
     else
-      'images/zip_file.png'
+      'zip_file.png'
     end
   end
 end
