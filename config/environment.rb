15c74bc0e74a1edde44b0f3d1c8184da7c7c80ca
Robert Walker <robert4723@me.com>
2010-06-17 10:25:05 -0400

    Change: Updated for new gem versions. Will_paginate 2.3.14 Cucumber 0.8.3 Cucumber-rails 0.3.2 Factory_girl 1.2.4

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
   config.gem 'aasm', :version => '>=2.1.5'
   config.gem 'RedCloth', :version => '>=4.2.3'
   config.gem 'gravtastic', :version => '>= 2.2.0'
-  config.gem 'will_paginate', :version => '>= 2.3.12'
+  config.gem 'will_paginate', :version => '>= 2.3.14'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
